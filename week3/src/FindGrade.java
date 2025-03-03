04929a32f0b9cefce476ea61397e4dc14c9305ac
Hasan Ali Özkan <hasanaliozkan@mu.edu.tr>
2025-03-03 22:16:00 +0300

    week3

---
+++ week3/src/FindGrade.java
@@ -0,0 +1,22 @@
+public class FindGrade{
+
+    public static void main(String[] args){
+
+        //Get the score from the user as a file parameter
+        //Calculate the grade based on the score
+        //Print the grade to the console
+        // 90 - 100 -> A
+        // 80 - 89 -> B
+        // 70 - 79 -> C
+        // 60 - 69 -> D
+        // 0 - 59 -> F
+
+
+
+
+
+    }
+
+
+
+}
